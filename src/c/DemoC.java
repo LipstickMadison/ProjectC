2540c4defc74978041a231b0c1df964d03c8861f
vanessawilliams <vanessawilliams@localhost>
2024-01-03 20:58:47 -0500

    initial commit

---
+++ src/c/DemoC.java
@@ -0,0 +1,14 @@
+package c;
+
+public class DemoC {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Wednesday");
+		
+		System.out.println("day 1");
+		System.out.println("day 2");
+		System.out.println("day 3");
+	}
+
+}
